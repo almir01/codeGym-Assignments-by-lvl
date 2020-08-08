e0c6d614adc062fd22cf37ba2bba59c05266717d
Almir <36572470+almir01@users.noreply.github.com>
2020-08-08 11:52:28 +0200

    Add files via upload

---
+++ codeGym lvl 0/Assigment_1.java
@@ -0,0 +1,16 @@
+package codeGym_lvl_0;
+
+
+//First exercise
+//Write a program that displays: "I think being a programmer is cool".
+
+
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I think being a programmer is cool");
+
+	}
+
+}
